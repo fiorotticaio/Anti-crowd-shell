43fca703998a6c66cde13781b404c641b4853f55
fiorotticaio <caiofiorotti@gmail.com>
2023-05-21 16:03:17 -0300

    :recycle: lendo varios comandos com seus argumentos

--- main.c
+++ main.c
@@ -2,46 +2,126 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 #define TAM_MAX_LINHA_DE_CMD 2000
+#define TAM_MAX_CMD 1000
 #define DELIMITADOR_COMANDO " <3 "
+#define DELIMITADOR_ARG " "
 
+/*========== Cabeçalho das funções ==========*/
+static void limpaTerminal();
+void leLinhaDeComando(char* comando);
+char* separaLinhaEmComandos(char* linhaDeComando, const char* delimitador);
+void executaComando(char* comando, char* argumentos[]);
+
+
+/*========== Main ==========*/
+int main(int argc, char **argv) {
+  int qtdMaxArgumentos = argv[1] != NULL ? atoi(argv[1]) : 3; 
+
+  char linhaDeComando[TAM_MAX_LINHA_DE_CMD], *token, *token2, *argumentos[qtdMaxArgumentos];
+
+  limpaTerminal();
+
+  pid_t pid = getpid();
+  pid_t spid = getsid(pid);
+  printf("PID do acsh: %d\n", pid);
+  printf("PID da sessão do acsh: %d\n", spid);
+
+  while (1) {
+    leLinhaDeComando(linhaDeComando);
+
+    setsid(); // Cria uma nova sessão para o processo filho
+
+    token = separaLinhaEmComandos(linhaDeComando, DELIMITADOR_COMANDO); // Separa a linha de comando com o delimitador
+    while (token != NULL) {
+      char cmd[TAM_MAX_CMD];
+      int i = 0, j = 0;
+      char* array[100];
+
+      /* Transformar a linha em palavras */
+      token2 = strtok(token, DELIMITADOR_ARG);
+      while (token2 != NULL) {
+        array[i] = strdup(token2);
+        token2 = strtok(NULL, DELIMITADOR_ARG);
+        i++;
+      }
+      strcpy(cmd, array[0]); // A primeira palavra é o comando em si
+
+      /* As outras palavras são os argumentos */
+      for (j = 0; j < i; j++) argumentos[j] = array[j];
+      argumentos[j] = NULL; // O último argumento é NULL
+
+      executaComando(cmd, argumentos);
+
+      token = separaLinhaEmComandos(NULL, DELIMITADOR_COMANDO); // Próximo comando
+
+      /* Liberar a memória alocada para cada palavra do comando */
+      for (j = 0; j < i; j++) free(array[j]); 
+    }
+
+  }
+
+  return 0;
+}
+
+
+/*========== Implementação das funções ==========*/
 static void limpaTerminal() {
   printf("\033[2J\033[H"); // clear
 }
 
-void leComando(char* comando) {
+void leLinhaDeComando(char* comando) {
   printf("acsh> ");
   fgets(comando, TAM_MAX_LINHA_DE_CMD, stdin);
   comando[strcspn(comando, "\n")] = '\0';  // Remover o caractere de nova linha
 }
 
-void executaComando(char* comando) {
+// TODO: Entender essa função obscura do nosso amigo Chat 
+char* separaLinhaEmComandos(char* linhaDeComando, const char* delimitador) {
+  static char* token = NULL;
+  if (linhaDeComando != NULL) token = linhaDeComando;
+  if (token == NULL) return NULL;
+
+  char* posDelimitador = strstr(token, delimitador);
+  if (posDelimitador == NULL) {
+    char* tokenAtual = token;
+    token = NULL;
+    return tokenAtual;
+  }
+
+  *posDelimitador = '\0';
+  char* tokenAtual = token;
+  token = posDelimitador + strlen(delimitador);
+  return tokenAtual;
+}
+
+void executaComando(char* comando, char* argumentos[]) {
   if (strcmp(comando, "exit") == 0) {
     exit(0); // Finaliza o programa
   }
 
-  // Implemente aqui a lógica para executar o comando
-  // Você pode usar funções como execvp() ou system()
-  // Exemplo básico:
-  printf("%s\n", comando);
-  system(comando);
-}
+  /* Cria um processo separado para executar o comando */
+  pid_t pid = fork();
+  if (pid < 0) {
+    printf("Erro ao criar processo\n");
+    exit(1);
 
-int main(int argc, char **argv) {
-  char linhaDeComando[TAM_MAX_LINHA_DE_CMD];
-  char* token;
+  } else if (pid == 0) { // Processo filho
+    pid_t spid = getsid(getpid());
+    printf("PID do processo filho: %d\n", getpid());
+    printf("PID da sessão do processo filho: %d\n", spid);
 
-  limpaTerminal();
-  while (1) {
-    leComando(linhaDeComando);
+    /* Executar o comando no processo filho */
+    execvp(comando, argumentos);
 
-    token = strtok(linhaDeComando, DELIMITADOR_COMANDO); // Separa a linha de comando com o delimitador
-    while (token != NULL) {
-      executaComando(token);
-      token = strtok(NULL, DELIMITADOR_COMANDO);
-    }
+    /* Se execlp retornar, houve um erro */
+    fprintf(stderr, "Erro ao executar o comando: %s\n", comando);
+    exit(1);
+
+  } else { // Processo pai
+    int status;
+    waitpid(pid, &status, 0);
   }
-
-  return 0;
 }
