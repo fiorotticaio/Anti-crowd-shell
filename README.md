a170b87aa6a62e0084cd411f8e712460ce62cd55
viniciuscole <viniamorim87@hotmail.com>
2023-06-09 13:45:00 -0300

    adicionando alguns bugs encontrados no readme, e tratando a saída do processo filho

--- README.md
+++ README.md
@@ -2,25 +2,27 @@
 
 ## IMPLEMENTAÇÕES PENDENTES
 
-Nesas seção marque as funcionalidades já implementadas e adicione as que ainda faltam ser implementadas
+Nessas seção marque as funcionalidades já implementadas e adicione as que ainda faltam ser implementadas
 
 - [ ] não está sendo tratado o sinal caso um filho tenha algum erro.
-    Por exemplo, se o filho faz um exit(1), o pai não está captando com WEXITSTATUS
-    para saber o que houve...
+      Por exemplo, se o filho faz um exit(1), o pai não está captando com WEXITSTATUS
+      para saber o que houve...
 
 ## BUGS
 
 Nessa seção marque os problemas que foram resolvidos e adicione os problemas ainda a resolver
 
 - [x] Faltando 1 free no valgrind
-- [ ] Processos filho devem ser todos de uma mesma seção, diferente do pai 
+- [ ] Processos filho devem ser todos de uma mesma seção, diferente do pai
 - [x] o sinal Ctrl+D envia o caracter EOF para o acsh, e ai entra em loop infinito
-- [ ] usar o comando `ls acsh.c acsh.h acsh.o main.c %` faz com que o acsh saia 
-    sem nenhuma mensagem de log nem nada aparentemente esse comportamento acontece
-    quando você passa mais de 3 argumentos para o ls
+- [ ] usar o comando `ls acsh.c acsh.h acsh.o main.c %` faz com que o acsh saia
+      sem nenhuma mensagem de log nem nada aparentemente esse comportamento acontece
+      quando você passa mais de 3 argumentos para o ls
 - [ ] sinais enviados ao acsh quando há processo em foreground devem passar pelo
-    proceso antes, mas estão sendo enviados direto ao acsh
+      proceso antes, mas estão sendo enviados direto ao acsh
 - [ ] o comando sleep não está sendo executado em background
+- [ ] o sinal Ctrl+D não é tratado caso seja utilizado juntamento com outros textos,
+      e ele faz com que infinitos processos sejam criados
 
 ## EXTRA
 
--- acsh.c
+++ acsh.c
@@ -1,32 +1,37 @@
 #include "acsh.h"
 
-void limpaTerminalAcsh() {
+void limpaTerminalAcsh()
+{
   printf("\033[2J\033[H"); // clear
 }
 
-int leLinhaDeComandoAcsh(char* comando) {
+int leLinhaDeComandoAcsh(char *comando)
+{
   printf("acsh> ");
 
   fgets(comando, TAM_MAX_LINHA_DE_CMD, stdin);
-  
+
   int qtdComandos = contaComandosAcsh(comando, DELIMITADOR_COMANDO);
-  if (qtdComandos > QTD_MAX_COMANDOS) {
+  if (qtdComandos > QTD_MAX_COMANDOS)
+  {
     fprintf(stderr, "Não é possível executar mais de %d comandos por vez\n", QTD_MAX_COMANDOS);
     return 0;
   }
 
   // Tratando quando o usuário usa Ctrl+D que é quando ele envia um EOF para o acsh
-  if (!comando || strlen(comando) == 0) {
+  if (!comando || strlen(comando) == 0)
+  {
     fprintf(stderr, "O comando é uma string NULA, saindo do acsh...\n");
     exit(1);
   }
 
-  comando[strcspn(comando, "\n")] = '\0';  // Remover o caractere de nova linha
+  comando[strcspn(comando, "\n")] = '\0'; // Remover o caractere de nova linha
 
   return 1;
 }
 
-int contaComandosAcsh(char* linhaDeComando, const char* delimitador) {
+int contaComandosAcsh(char *linhaDeComando, const char *delimitador)
+{
   int count = 0;
   int seqLen = strlen(delimitador);
   int cmdLen = strlen(linhaDeComando);
@@ -35,37 +40,41 @@ int contaComandosAcsh(char* linhaDeComando, const char* delimitador) {
     return 0;
 
   /* Percorre a linha de comando contando quando delimitadores tem */
-  for (int i = 0; i <= cmdLen - seqLen; i++) {
+  for (int i = 0; i <= cmdLen - seqLen; i++)
+  {
     if (strncmp(linhaDeComando + i, delimitador, seqLen) == 0)
       count++;
   }
 
-  return count+2;
+  return count + 2;
 }
 
-int trataLinhaDeComandoAcsh(char* linhaDeComando, int qtdMaxArgumentos) {
-  
+int trataLinhaDeComandoAcsh(char *linhaDeComando, int qtdMaxArgumentos)
+{
+
   pid_t pid = fork(); // Cria um processo separado para executar o(s) comando(s)
 
-  if (pid < 0) {
+  if (pid < 0)
+  {
     printf("Erro ao criar processo\n");
     exit(1);
-
-  } else if (pid == 0) { // Processo filho
+  }
+  else if (pid == 0)
+  { // Processo filho
 
     pid_t novoSIdFilho = setsid(); // Criar uma nova sessão para o processo filho
 
     char *token, *token2, *argumentos[qtdMaxArgumentos];
-    token = separaLinhaEmComandosAcsh(linhaDeComando, DELIMITADOR_COMANDO); // Separa a linha de comando com o delimitador
-
-    while (token != NULL) {
+    while (token != NULL)
+    {
       char cmd[TAM_MAX_CMD];
       int i = 0, j = 0;
-      char * array[100];
+      char *array[100];
 
       /* Transformar a linha em palavras */
       token2 = strtok(token, DELIMITADOR_ARG);
-      while (token2 != NULL) {
+      while (token2 != NULL)
+      {
         array[i] = strdup(token2);
         token2 = strtok(NULL, DELIMITADOR_ARG);
         i++;
@@ -73,77 +82,91 @@ int trataLinhaDeComandoAcsh(char* linhaDeComando, int qtdMaxArgumentos) {
       strcpy(cmd, array[0]); // A primeira palavra é o comando em si
 
       /* As outras palavras são os argumentos */
-      for (j = 0; j < i; j++) argumentos[j] = array[j];
+      for (j = 0; j < i; j++)
+        argumentos[j] = array[j];
       argumentos[j] = NULL; // O último argumento é NULL
 
       executaComandoAcsh(cmd, argumentos, array, i);
       token = separaLinhaEmComandosAcsh(NULL, DELIMITADOR_COMANDO); // Próximo comando
 
       /* Liberar a memória alocada para cada palavra do comando */
-      for (j = 0; j < i; j++) free(array[j]); 
-
-      
+      for (j = 0; j < i; j++)
+        free(array[j]);
     }
-
-  } else { // Processo pai
+  }
+  else
+  { // Processo pai
     int status;
     waitpid(pid, &status, 0); // Esperar o processo filho terminar
     return status;
   }
 }
 
-char* separaLinhaEmComandosAcsh(char* linhaDeComando, const char* delimitador) {
-  static char* token = NULL;
-  if (linhaDeComando != NULL && strcmp(linhaDeComando, "\n")!=0) token = linhaDeComando;
-  if (token == NULL) return NULL;
+char *separaLinhaEmComandosAcsh(char *linhaDeComando, const char *delimitador)
+{
+  static char *token = NULL;
+  if (linhaDeComando != NULL && strcmp(linhaDeComando, "\n") != 0)
+    token = linhaDeComando;
+  if (token == NULL)
+    return NULL;
 
-  char* posDelimitador = strstr(token, delimitador);
-  if (posDelimitador == NULL) {
-    char* tokenAtual = token;
+  char *posDelimitador = strstr(token, delimitador);
+  if (posDelimitador == NULL)
+  {
+    char *tokenAtual = token;
     token = NULL;
     return tokenAtual;
   }
 
   *posDelimitador = '\0';
-  char* tokenAtual = token;
+  char *tokenAtual = token;
   token = posDelimitador + strlen(delimitador);
   return tokenAtual;
 }
 
-void executaComandoAcsh(char* comando, char* argumentos[], char * array[], int sizeArray) {
+void executaComandoAcsh(char *comando, char *argumentos[], char *array[], int sizeArray)
+{
 
-  if (strcmp(comando, "exit") == 0) {
+  if (strcmp(comando, "exit") == 0)
+  {
     /* Caso seja exit, saia do programa */
-    for (int j = 0; j < sizeArray; j++) free(array[j]); 
-    exit(0); 
-
-  } else if (strcmp(comando, "cd") == 0) {
-    /* 
+    for (int j = 0; j < sizeArray; j++)
+      free(array[j]);
+    exit(0);
+  }
+  else if (strcmp(comando, "cd") == 0)
+  {
+    /*
       Caso o comando seja cd, nenhum processo filho é criado,
       apenas alteramos o diretório de trabalho do processo pai
     */
     int i = 0;
-    while (argumentos[i] != NULL) i++;
-    
-    const char* homeDir = getenv("HOME"); // Vai pra pasta home caso não tenha argumentos
-    if (!argumentos[1]) chdir(homeDir);
+    while (argumentos[i] != NULL)
+      i++;
 
-    chdir(argumentos[i-1]);
+    const char *homeDir = getenv("HOME"); // Vai pra pasta home caso não tenha argumentos
+    if (!argumentos[1])
+      chdir(homeDir);
+
+    chdir(argumentos[i - 1]);
     return;
-  } 
+  }
 
   pid_t pid = fork(); // Cria um processo separado para executar o comando
 
-  if (pid < 0) {
+  if (pid < 0)
+  {
     printf("Erro ao criar processo\n");
     exit(1);
+  }
+  else if (pid == 0)
+  { // Processo filho
 
-  } else if (pid == 0) { // Processo filho
-
-    if (!executaEmForegroundAcsh(comando, argumentos)) {
-      /* 
-        Redirecionar entrada, saída e saída de erro para /dev/null, 
-        para executar em background 
+    if (!executaEmForegroundAcsh(comando, argumentos))
+    {
+      /*
+        Redirecionar entrada, saída e saída de erro para /dev/null,
+        para executar em background
       */
       int devnull = open("/dev/null", O_RDWR);
       dup2(devnull, STDIN_FILENO);
@@ -156,25 +179,36 @@ void executaComandoAcsh(char* comando, char* argumentos[], char * array[], int s
     execvp(comando, argumentos);
 
     /* Se execlp retornar, houve um erro */
-    fprintf(stderr, "Erro ao executar o comando: %s\n", comando);
-    exit(1); //TODO: esse sinal não está sendo tratado pelo processo PAI
-
-  } else if (pid > 0) {
+    exit(1);
+  }
+  else if (pid > 0)
+  {
     int status;
-    waitpid(pid, &status, 0); // Esperar o processo filho terminar
+    while (wait(&status) != -1)
+    {
+      if (WIFEXITED(status))
+      {
+        if (WEXITSTATUS(status) == 1)
+          printf("Comando %s não encontrado\n", comando); // Esse é o tratamento?
+      }
+    }
   }
 }
 
-int executaEmForegroundAcsh(char* comando, char* argumentos[]) {
+int executaEmForegroundAcsh(char *comando, char *argumentos[])
+{
   int i = 0;
-  while (argumentos[i] != NULL) i++;
+  while (argumentos[i] != NULL)
+    i++;
 
   /* Comandos internos devem ser executados em foreground sempre */
-  if (strcmp(comando, "exit") == 0 || strcmp(comando, "cd") == 0) return 1;
+  if (strcmp(comando, "exit") == 0 || strcmp(comando, "cd") == 0)
+    return 1;
 
   /* Se o último argumento for "%", executar em foreground */
-  if (i > 0 && strcmp(argumentos[i-1], "%") == 0) {
-    argumentos[i-1] = NULL;
+  if (i > 0 && strcmp(argumentos[i - 1], "%") == 0)
+  {
+    argumentos[i - 1] = NULL;
     return 1;
   }
 
